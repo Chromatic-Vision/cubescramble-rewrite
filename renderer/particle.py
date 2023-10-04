f7725b2aa8383b134f98d3d8aff4e5e4d4992754
huu-bo <huubhoogweg@gmail.com>
2023-10-04 20:22:30 +0200

    particles render with alpha

--- renderer/particle.py
+++ renderer/particle.py
@@ -9,13 +9,16 @@ class ParticleRenderer:
         self.screen = screen
         self.particles = []
 
-    def connect_lines(self):
+    def connect_lines(self, screen):
         for i in range(len(self.particles)):
             for j in range(i + 1, len(self.particles)):
                 if self.particles[i].distance_to(self.particles[j]) < 100:
-                    print(self.particles[i].color)
-                    pygame.draw.line(self.screen, self.particles[i].color, (self.particles[i].x, self.particles[i].y),
-                                     (self.particles[j].x, self.particles[j].y), 1)
+                    # print(self.particles[i].color)
+                    # pygame.draw.line(screen, self.particles[i].color, (self.particles[i].x, self.particles[i].y),
+                    #                  (self.particles[j].x, self.particles[j].y), 10)
+                    # TODO: pygame wiki says blend is deprecated
+                    pygame.draw.aaline(screen, self.particles[i].color, (self.particles[i].x, self.particles[i].y),
+                                       (self.particles[j].x, self.particles[j].y), 1)
 
     def clear(self):
         for p in self.particles:
@@ -29,23 +32,24 @@ class ParticleRenderer:
             self.particles.append(ParticleRenderer.Particle(self.screen.get_size()))
 
     def update(self):
+        s = pygame.Surface(self.screen.get_size(), pygame.SRCALPHA)
         for p in self.particles:
             if p.fadeout:
                 p.color = (
                     p.color[0] - 10,
                     p.color[1] - 10,
-                    p.color[2] - 10
+                    p.color[2] - 10,
+                    p.color[3] - 10,
                 )
-                if p.color[0] <= 0 \
-                        or p.color[1] <= 0 \
-                        or p.color[2] <= 0:
+                if min(p.color) <= 0:
                     self.particles.remove(p)
                     continue
 
             p.move()
-            p.render(self.screen)
+            p.render(s)
 
-        self.connect_lines()
+        self.connect_lines(s)
+        self.screen.blit(s, (0, 0))
 
     class Particle:
 
@@ -67,7 +71,7 @@ class ParticleRenderer:
             self.vx = random.uniform(-2.5, 2.5)
             self.vy = random.uniform(-2.5, 2.5)
             # self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-            self.color = (255, 255, 255)
+            self.color = (255, 255, 255, 100)
             self.fadeout = False
 
         def render(self, screen: pygame.surface.Surface):
