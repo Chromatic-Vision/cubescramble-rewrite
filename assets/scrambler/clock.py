610a7960b4ce34a3e1c00543cbe96ac9bdb1df8e
Teru Kaazhooft <n401571@gymnasiumnovum.nl>
2023-10-10 21:29:51 +0200

    Backup & WIP Clock scrambler

--- assets/scrambler/clock.py
+++ assets/scrambler/clock.py
@@ -1,7 +1,7 @@
 import random
 
-def get_scramble():
 
+def get_scramble():
     scramble = ""
 
     scramble += "UR" + random_rotation() + " "
@@ -23,8 +23,8 @@ def get_scramble():
 
     return scramble
 
-def random_rotation():
 
+def random_rotation():
     res = ""
     number = random.randint(0, 6)
 
@@ -40,8 +40,8 @@ def random_rotation():
 
     return res
 
-def random_pins():
 
+def random_pins():
     res = ""
 
     for i in range(4):
@@ -56,3 +56,120 @@ def random_pins():
                 res += " UL"
 
     return res
+
+
+class Clock:
+
+    def __init__(self):
+        front = Clock.Side(True)
+        back = Clock.Side(False)
+
+    class Side:
+        MOVE_STATE = [
+            [0, 1, 1,
+             0, 1, 1,
+             0, 0, 0,
+
+             -1, 0, 0,
+             0, 0, 0,
+             0, 0, 0
+             ],  # UR
+
+            [0, 0, 0,
+             0, 1, 1,
+             0, 1, 1,
+
+             0, 0, 0,
+             0, 0, 0,
+             -1, 0, 0
+             ],  # DR
+
+            [0, 0, 0,
+             1, 1, 0,
+             1, 1, 0,
+
+             0, 0, 0,
+             0, 0, 0,
+             0, 0, -1
+             ],  # DL
+
+            [1, 1, 0,
+             1, 1, 0,
+             0, 0, 0,
+
+             0, 0, -1,
+             0, 0, 0,
+             0, 0, 0
+             ],  # UL
+
+            [1, 1, 1,
+             1, 1, 1,
+             0, 0, 0,
+
+             -1, 0, -1,
+             0, 0, 0,
+             0, 0, 0
+             ],  # U
+
+            [0, 1, 1,
+             0, 1, 1,
+             0, 1, 1,
+
+             -1, 0, 0,
+             0, 0, 0,
+             -1, 0, 0
+             ],  # R
+
+            [0, 0, 0,
+             1, 1, 1,
+             1, 1, 1,
+
+             0, 0, 0,
+             0, 0, 0,
+             -1, 0, -1
+             ],  # D
+
+            [1, 1, 0,
+             1, 1, 0,
+             1, 1, 0,
+
+             0, 0, -1,
+             0, 0, 0,
+             0, 0, -1
+             ],  # L
+
+            [1, 1, 1,
+             1, 1, 1,
+             1, 1, 1,
+
+             -1, 0, -1,
+             0, 0, 0,
+             -1, 0, -1
+             ],  # ALL
+
+            [0, 1, 1,
+             1, 1, 1,
+             1, 1, 0,
+
+             -1, 0, 0,
+             0, 0, 0,
+             0, 0, -1
+             ],  # /
+
+            [1, 1, 0,
+             1, 1, 1,
+             0, 1, 1,
+
+             0, 0, -1,
+             0, 0, 0,
+             -1, 0, 0
+             ],  # \
+        ]
+
+        def __init__(self, front):
+            self.front = front
+            self.pins = [1, 1,
+                         1, 1]
+            self.states = [0, 0, 0,
+                           0, 0, 0,
+                           0, 0, 0]
