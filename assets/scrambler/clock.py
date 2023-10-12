d173554595b9f7f3d5556b8e76ff0c7022cf3599
Teru Kaazhooft <n401571@gymnasiumnovum.nl>
2023-10-12 13:10:59 +0200

    Backup & WIP Clock scrambler

--- assets/scrambler/clock.py
+++ assets/scrambler/clock.py
@@ -61,11 +61,10 @@ def random_pins():
 class Clock:
 
     def __init__(self):
-        front = Clock.Side(True)
-        back = Clock.Side(False)
+        self.front = Clock.Side(True)
+        self.back = Clock.Side(False)
 
-    class Side:
-        MOVE_STATE = [
+        self.MOVE_STATE = [
             [0, 1, 1,
              0, 1, 1,
              0, 0, 0,
@@ -166,6 +165,8 @@ class Clock:
              ],  # \
         ]
 
+    class Side:
+
         def __init__(self, front):
             self.front = front
             self.pins = [1, 1,
@@ -173,3 +174,142 @@ class Clock:
             self.states = [0, 0, 0,
                            0, 0, 0,
                            0, 0, 0]
+
+            self.MOVE_STATE = [
+                [0, 1, 1,
+                 0, 1, 1,
+                 0, 0, 0,
+
+                 -1, 0, 0,
+                 0, 0, 0,
+                 0, 0, 0
+                 ],  # UR, 0
+
+                [0, 0, 0,
+                 0, 1, 1,
+                 0, 1, 1,
+
+                 0, 0, 0,
+                 0, 0, 0,
+                 -1, 0, 0
+                 ],  # DR, 1
+
+                [0, 0, 0,
+                 1, 1, 0,
+                 1, 1, 0,
+
+                 0, 0, 0,
+                 0, 0, 0,
+                 0, 0, -1
+                 ],  # DL, 2
+
+                [1, 1, 0,
+                 1, 1, 0,
+                 0, 0, 0,
+
+                 0, 0, -1,
+                 0, 0, 0,
+                 0, 0, 0
+                 ],  # UL, 3
+
+                [1, 1, 1,
+                 1, 1, 1,
+                 0, 0, 0,
+
+                 -1, 0, -1,
+                 0, 0, 0,
+                 0, 0, 0
+                 ],  # U, 4
+
+                [0, 1, 1,
+                 0, 1, 1,
+                 0, 1, 1,
+
+                 -1, 0, 0,
+                 0, 0, 0,
+                 -1, 0, 0
+                 ],  # R, 5
+
+                [0, 0, 0,
+                 1, 1, 1,
+                 1, 1, 1,
+
+                 0, 0, 0,
+                 0, 0, 0,
+                 -1, 0, -1
+                 ],  # D, 6
+
+                [1, 1, 0,
+                 1, 1, 0,
+                 1, 1, 0,
+
+                 0, 0, -1,
+                 0, 0, 0,
+                 0, 0, -1
+                 ],  # L, 7
+
+                [1, 1, 1,
+                 1, 1, 1,
+                 1, 1, 1,
+
+                 -1, 0, -1,
+                 0, 0, 0,
+                 -1, 0, -1
+                 ],  # ALL, 8
+
+                [0, 1, 1,
+                 1, 1, 1,
+                 1, 1, 0,
+
+                 -1, 0, 0,
+                 0, 0, 0,
+                 0, 0, -1
+                 ],  # /, 9
+
+                [1, 1, 0,
+                 1, 1, 1,
+                 0, 1, 1,
+
+                 0, 0, -1,
+                 0, 0, 0,
+                 -1, 0, 0
+                 ],  # \, 10
+            ]
+
+        def move(self, amount, pins: [int, int, int, int]):
+            if pins[0] and pins[1]:
+                self.move_with(amount, 4)
+            elif pins[0] and pins[2]:
+                self.move_with(amount, 7)
+            elif pins[0] and pins[3]:
+                self.move_with(amount, 10)
+            elif pins[1] and pins[2]:
+                self.move_with(amount, 9)
+            elif pins[1] and pins[3]:
+                self.move_with(amount, 5)
+            elif pins[2] and pins[3]:
+                self.move_with(amount, 6)
+            elif pins[0] and not pins[1] and not pins[2] and not pins[3]:
+                self.move_with(amount, 3)
+            elif not pins[0] and pins[1] and not pins[2] and not pins[3]:
+                self.move_with(amount, 0)
+            elif not pins[0] and not pins[1] and pins[2] and not pins[3]:
+                self.move_with(amount, 2)
+            elif not pins[0] and not pins[1] and not pins[2] and pins[3]:
+                self.move_with(amount, 1)
+            elif pins[0] and pins[1] and pins[2] and pins[3]:
+                self.move_with(amount, 8)
+
+
+
+        def move_with(self, amount, move_index):
+            move_rule = self.MOVE_STATE[move_index]
+
+            for i in range(move_rule):
+
+                rule = move_rule[i]
+
+                if rule == 1:
+                    self.states[i] += amount
+                elif rule == -1:
+                    self.states[i] -= amount
