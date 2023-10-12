9c796421d05e623322152fff3733cf181000da9e
Teru Kaazhooft <n401571@gymnasiumnovum.nl>
2023-10-12 15:32:24 +0200

    HEHEHEHAW

--- assets/scrambler/clock.py
+++ assets/scrambler/clock.py
@@ -61,8 +61,6 @@ def random_pins():
 class Clock:
 
     def __init__(self):
-        self.front = Clock.Side(True)
-        self.back = Clock.Side(False)
 
         self.MOVE_STATE = [
             [0, 1, 1,
@@ -72,7 +70,7 @@ class Clock:
              -1, 0, 0,
              0, 0, 0,
              0, 0, 0
-             ],  # UR
+             ],  # UR, 0
 
             [0, 0, 0,
              0, 1, 1,
@@ -81,7 +79,7 @@ class Clock:
              0, 0, 0,
              0, 0, 0,
              -1, 0, 0
-             ],  # DR
+             ],  # DR, 1
 
             [0, 0, 0,
              1, 1, 0,
@@ -90,7 +88,7 @@ class Clock:
              0, 0, 0,
              0, 0, 0,
              0, 0, -1
-             ],  # DL
+             ],  # DL, 2
 
             [1, 1, 0,
              1, 1, 0,
@@ -99,7 +97,7 @@ class Clock:
              0, 0, -1,
              0, 0, 0,
              0, 0, 0
-             ],  # UL
+             ],  # UL, 3
 
             [1, 1, 1,
              1, 1, 1,
@@ -108,7 +106,7 @@ class Clock:
              -1, 0, -1,
              0, 0, 0,
              0, 0, 0
-             ],  # U
+             ],  # U, 4
 
             [0, 1, 1,
              0, 1, 1,
@@ -117,7 +115,7 @@ class Clock:
              -1, 0, 0,
              0, 0, 0,
              -1, 0, 0
-             ],  # R
+             ],  # R, 5
 
             [0, 0, 0,
              1, 1, 1,
@@ -126,7 +124,7 @@ class Clock:
              0, 0, 0,
              0, 0, 0,
              -1, 0, -1
-             ],  # D
+             ],  # D, 6
 
             [1, 1, 0,
              1, 1, 0,
@@ -135,7 +133,7 @@ class Clock:
              0, 0, -1,
              0, 0, 0,
              0, 0, -1
-             ],  # L
+             ],  # L, 7
 
             [1, 1, 1,
              1, 1, 1,
@@ -144,7 +142,7 @@ class Clock:
              -1, 0, -1,
              0, 0, 0,
              -1, 0, -1
-             ],  # ALL
+             ],  # ALL, 8
 
             [0, 1, 1,
              1, 1, 1,
@@ -153,7 +151,7 @@ class Clock:
              -1, 0, 0,
              0, 0, 0,
              0, 0, -1
-             ],  # /
+             ],  # /, 9
 
             [1, 1, 0,
              1, 1, 1,
@@ -162,154 +160,72 @@ class Clock:
              0, 0, -1,
              0, 0, 0,
              -1, 0, 0
-             ],  # \
+             ],  # \, 10
         ]
+
+        self.pins = [True, True,
+                     True, True]
+
+        self.front = Clock.Side(True)
+        self.back = Clock.Side(False)
+
+
+
+    def move(self, amount, pins: [int, int, int, int]):
+
+        if pins[0] and pins[1] and pins[2] and pins[3]:
+            self.move_with(amount, 8)
+        elif pins[0] and not pins[1] and not pins[2] and not pins[3]:
+            self.move_with(amount, 3)
+        elif not pins[0] and pins[1] and not pins[2] and not pins[3]:
+            self.move_with(amount, 0)
+        elif not pins[0] and not pins[1] and pins[2] and not pins[3]:
+            self.move_with(amount, 2)
+        elif not pins[0] and not pins[1] and not pins[2] and pins[3]:
+            self.move_with(amount, 1)
+        elif pins[0] and pins[1] and not pins[2] and not pins[3]:
+            self.move_with(amount, 4)
+        elif pins[0] and pins[2] and not pins[1] and not pins[3]:
+            self.move_with(amount, 7)
+        elif pins[0] and pins[3] and not pins[1] and not pins[2]:
+            self.move_with(amount, 10)
+        elif pins[1] and pins[2] and not pins[3] and not pins[0]:
+            self.move_with(amount, 9)
+        elif pins[1] and pins[3] and not pins[0] and not pins[2]:
+            self.move_with(amount, 5)
+        elif pins[2] and pins[3] and not pins[0] and not pins[1]:
+            self.move_with(amount, 6)
+        else:
+            print("not implemented")
+
+    def move_with(self, amount, move_index):
+        move_rule = self.MOVE_STATE[move_index]
+
+        for i in range(move_rule.__len__()):
+
+            rule = move_rule[i]
+
+            if i < 9:
+                if rule == 1:
+                    self.front.states[i] += amount
+                #elif rule == -1:
+                    #self.front.states[i] -= amount
+                    #print("how????")
+
+            else:
+                if rule == -1:
+                    self.back.states[i - 9] -= amount
+                else:
+                    print("how????")
+
+            #self.front.states[i] %= 12
+            #self.back.states[i] %= 12
+
 
     class Side:
 
         def __init__(self, front):
             self.front = front
-            self.pins = [1, 1,
-                         1, 1]
             self.states = [0, 0, 0,
                            0, 0, 0,
                            0, 0, 0]
-
-            self.MOVE_STATE = [
-                [0, 1, 1,
-                 0, 1, 1,
-                 0, 0, 0,
-
-                 -1, 0, 0,
-                 0, 0, 0,
-                 0, 0, 0
-                 ],  # UR, 0
-
-                [0, 0, 0,
-                 0, 1, 1,
-                 0, 1, 1,
-
-                 0, 0, 0,
-                 0, 0, 0,
-                 -1, 0, 0
-                 ],  # DR, 1
-
-                [0, 0, 0,
-                 1, 1, 0,
-                 1, 1, 0,
-
-                 0, 0, 0,
-                 0, 0, 0,
-                 0, 0, -1
-                 ],  # DL, 2
-
-                [1, 1, 0,
-                 1, 1, 0,
-                 0, 0, 0,
-
-                 0, 0, -1,
-                 0, 0, 0,
-                 0, 0, 0
-                 ],  # UL, 3
-
-                [1, 1, 1,
-                 1, 1, 1,
-                 0, 0, 0,
-
-                 -1, 0, -1,
-                 0, 0, 0,
-                 0, 0, 0
-                 ],  # U, 4
-
-                [0, 1, 1,
-                 0, 1, 1,
-                 0, 1, 1,
-
-                 -1, 0, 0,
-                 0, 0, 0,
-                 -1, 0, 0
-                 ],  # R, 5
-
-                [0, 0, 0,
-                 1, 1, 1,
-                 1, 1, 1,
-
-                 0, 0, 0,
-                 0, 0, 0,
-                 -1, 0, -1
-                 ],  # D, 6
-
-                [1, 1, 0,
-                 1, 1, 0,
-                 1, 1, 0,
-
-                 0, 0, -1,
-                 0, 0, 0,
-                 0, 0, -1
-                 ],  # L, 7
-
-                [1, 1, 1,
-                 1, 1, 1,
-                 1, 1, 1,
-
-                 -1, 0, -1,
-                 0, 0, 0,
-                 -1, 0, -1
-                 ],  # ALL, 8
-
-                [0, 1, 1,
-                 1, 1, 1,
-                 1, 1, 0,
-
-                 -1, 0, 0,
-                 0, 0, 0,
-                 0, 0, -1
-                 ],  # /, 9
-
-                [1, 1, 0,
-                 1, 1, 1,
-                 0, 1, 1,
-
-                 0, 0, -1,
-                 0, 0, 0,
-                 -1, 0, 0
-                 ],  # \, 10
-            ]
-
-        def move(self, amount, pins: [int, int, int, int]):
-            if pins[0] and pins[1]:
-                self.move_with(amount, 4)
-            elif pins[0] and pins[2]:
-                self.move_with(amount, 7)
-            elif pins[0] and pins[3]:
-                self.move_with(amount, 10)
-            elif pins[1] and pins[2]:
-                self.move_with(amount, 9)
-            elif pins[1] and pins[3]:
-                self.move_with(amount, 5)
-            elif pins[2] and pins[3]:
-                self.move_with(amount, 6)
-            elif pins[0] and not pins[1] and not pins[2] and not pins[3]:
-                self.move_with(amount, 3)
-            elif not pins[0] and pins[1] and not pins[2] and not pins[3]:
-                self.move_with(amount, 0)
-            elif not pins[0] and not pins[1] and pins[2] and not pins[3]:
-                self.move_with(amount, 2)
-            elif not pins[0] and not pins[1] and not pins[2] and pins[3]:
-                self.move_with(amount, 1)
-            elif pins[0] and pins[1] and pins[2] and pins[3]:
-                self.move_with(amount, 8)
-
-
-
-        def move_with(self, amount, move_index):
-            move_rule = self.MOVE_STATE[move_index]
-
-            for i in range(move_rule):
-
-                rule = move_rule[i]
-
-                if rule == 1:
-                    self.states[i] += amount
-                elif rule == -1:
-                    self.states[i] -= amount
---
+++ assets/scrambler/test.py
@@ -0,0 +1,55 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 800), pygame.RESIZABLE)
+
+FONT_SIZE = 50
+font = pygame.font.SysFont("bahnschrift", 50)
+
+arfaere = pygame.time.Clock()
+from assets.scrambler.clock import Clock
+
+clock = Clock()
+
+while 1:
+
+    screen.fill((0, 0, 0))
+
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            exit(0)
+
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+
+            fy = 500
+
+            for i in range(clock.pins.__len__()):
+                if fy <= ev.pos[1] <= fy + FONT_SIZE:
+                    clock.pins[i] = True if clock.pins[i] == False else False
+
+                fy += FONT_SIZE
+
+        if ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_RIGHT:
+                clock.move(1, clock.pins)
+            elif ev.key == pygame.K_LEFT:
+                clock.move(-1, clock.pins)
+
+
+    for i in range(clock.front.states.__len__()):
+        pygame.draw.rect(screen, (64, 77, 255), (i % 3 * 100, i // 3 * 100, 75, 75))
+        screen.blit(font.render(str(clock.front.states[i]), True, (255, 255, 255)), (i % 3 * 100 + 30, i // 3 * 100 + 30))
+
+    for i in range(clock.back.states.__len__()):
+        pygame.draw.rect(screen, (214, 77, 188), (i % 3 * 100 + 400, i // 3 * 100, 75, 75))
+        screen.blit(font.render(str(clock.back.states[i]), True, (255, 255, 255)), (i % 3 * 100 + 30 + 400, i // 3 * 100 + 30))
+
+    fy = 500
+
+    for i in range(clock.pins.__len__()):
+        screen.blit(font.render(f"Pin {i}: {clock.pins[i]}", True,(255, 255, 255)), (20, fy))
+        fy += FONT_SIZE
+
+
+    arfaere.tick(60)
+    pygame.display.update()
