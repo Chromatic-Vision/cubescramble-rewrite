f70fc32c1e7af14bc56ca2ff9f8428570e81431c
Teru Kaazhooft <n401571@gymnasiumnovum.nl>
2023-12-02 20:35:47 +0100

    add pyraminx scrambler... but not working (why)

---
+++ assets/scrambler/pyraminx.py
@@ -0,0 +1,339 @@
+import random
+from enum import Enum
+
+moves = ["L", "R", "B", "U"]
+
+def get_scramble():
+
+    rm = []
+
+    for i in range(8):
+
+        move = random_move()
+
+        if len(rm) > 0: # if the scramble is not empty, check if the next base move equals to the previous one
+
+            while get_base_of_move(move) == get_base_of_move(rm[i - 1]):
+                move = random_move()
+
+        rm.append(move)
+
+    scramble = " ".join(rm) + random_tips()
+
+    return scramble
+
+def get_base_of_move(move: str):
+    res = move
+    res = res.replace(" ", "") # remove space
+    res = res.replace("'", "") # remove prime
+
+    return res
+
+def random_move():
+    return random.choice(moves) + prime_random()
+
+def random_tips():
+    res = ""
+
+    for i in range(4):
+        if bool(random.getrandbits(1)):
+            if i == 0:
+                res += " l"
+                res += prime_random()
+
+            if i == 1:
+                res += " r"
+                res += prime_random()
+
+            if i == 2:
+                res += " b"
+                res += prime_random()
+
+            if i == 3:
+                res += " u"
+                res += prime_random()
+
+    return res
+
+def prime_random():
+    if bool(random.getrandbits(1)):
+        return "'"
+    else:
+        return ""
+
+
+class Color:
+
+    GREEN = 0
+    RED = 1
+    BLUE = 2
+    YELLOW = 3
+
+PIECE_SIDES = 9
+
+class MoveMappings(Enum):
+
+    L = [ # index 0 = GREEN, 1 = RED, BLUE = 2, YELLOW = 3
+        [
+            -1,
+            1, -1, -1,
+            1, 1, 1, -1, -1 # Here it indicates that 4 pieces on the left side from red side will move to the green side. -1 means it won't move.
+        ],
+        [
+            -1,
+            -1, -1, 3,
+            -1, -1, 3, 3, 3
+        ],
+        [
+            -1,
+            -1, -1, -1,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            -1,
+            -1, -1, 0,
+            -1, -1, 0, 0, 0 # Yellow is a little bit weird because we flip the shape here...
+        ]
+    ]
+
+    L_PRIME = [
+        [
+            -1,
+            3, -1, -1,
+            3, 3, 3, -1, -1
+        ],
+        [
+            -1,
+            -1, -1, 0,
+            -1, -1, 0, 0, 0
+        ],
+        [
+            -1,
+            -1, -1, -1,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            -1,
+            -1, -1, 1,
+            -1, -1, 1, 1, 1
+        ]
+    ]
+
+    R = [
+        [
+            -1,
+            -1, -1, 3,
+            -1, -1, 3, 3, 3
+        ],
+        [
+            -1,
+            -1, -1, -1,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            -1,
+            0, -1, -1,
+            0, 0, 0, -1, -1
+        ],
+        [
+            -1,
+            2, -1, 1,
+            2, 2, 2, 1, 1
+        ]
+    ]
+
+    R_PRIME = [
+        [
+            -1,
+            -1, -1, 2,
+            -1, -1, 2, 2, 2
+        ],
+        [
+            -1,
+            -1, -1, -1,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            -1,
+            3, -1, -1,
+            3, 3, 3, -1, -1
+        ],
+        [
+            -1,
+            0, -1, 1,
+            0, 0, 0, 1, 1
+        ]
+    ]
+
+    B = [
+        [
+            -1,
+            -1, -1, -1,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            -1,
+            2, -1, -1,
+            2, 2, 2, -1, -1
+        ],
+        [
+            -1,
+            -1, -1, 3,
+            -1, -1, 3, 3, 3
+        ],
+        [
+            1,
+            1, 1, 1,
+            -1, -1, -1, -1, -1
+        ]
+    ]
+
+    B_PRIME = [
+        [
+            -1,
+            -1, -1, -1,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            -1,
+            3, -1, -1,
+            3, 3, 3, -1, -1
+        ],
+        [
+            -1,
+            -1, -1, 1,
+            -1, -1, 1, 1, 1
+        ],
+        [
+            2,
+            2, 2, 2,
+            -1, -1, -1, -1, -1
+        ]
+    ]
+
+    U = [
+        [
+            2,
+            2, 2, 2,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            0,
+            0, 0, 0,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            1,
+            1, 1, 1,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            -1,
+            -1, -1, -1,
+            -1, -1, -1, -1, -1
+        ]
+    ]
+
+    U_PRIME = [
+        [
+            1,
+            1, 1, 1,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            2,
+            2, 2, 2,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            0,
+            0, 0, 0,
+            -1, -1, -1, -1, -1
+        ],
+        [
+            -1,
+            -1, -1, -1,
+            -1, -1, -1, -1, -1
+        ]
+    ]
+
+class ColorSide:
+
+    def __init__(self, color: int):
+        self.color = color
+        self.piece_side = []
+
+    def reset(self):
+
+        self.piece_side = []
+
+        for i in range(PIECE_SIDES):
+            self.piece_side.append(self.color)
+
+
+class Pyraminx:
+
+    def __init__(self):
+        self.sides = [
+            ColorSide(Color.GREEN),
+            ColorSide(Color.RED),
+            ColorSide(Color.BLUE),
+            ColorSide(Color.YELLOW)
+        ]
+
+        for side in self.sides:
+            side.reset()
+
+    def move(self, notation):
+
+        if notation == "U":
+
+            lmap = MoveMappings.U.value
+            print(str(lmap))
+
+            copy_sides = self.sides.copy()
+
+            for i in range(len(self.sides)):
+
+                instructions = lmap[i]
+                print("instructions:", instructions)
+
+                side = self.sides[i]
+                print("current side color:", side.color)
+                print("ALL ORIGINS:")
+
+                for l in copy_sides:
+                    print(l.piece_side)
+
+                for j in range(len(side.piece_side)):
+
+                    origin_color = instructions[j]
+
+                    print(f"Color is now {origin_color}")
+
+                    if origin_color == -1:
+                        continue
+
+                    origin_side = copy_sides[origin_color]
+                    print("origin, unchangeable", origin_side.piece_side)
+                    print("real:", side.piece_side)
+
+                    origin_piece = origin_side.piece_side[j]
+
+                    print(f"setting piece {j} with value {side.piece_side[j]} to {origin_piece}")
+                    side.piece_side[j] = origin_piece
+
+
+if __name__ == "__main__":
+    # for _ in range(5):
+    #     print(get_scramble())
+
+    pm = Pyraminx()
+    for n, s in enumerate(pm.sides):
+        print(s.piece_side, n)
+    print("<<b")
+    pm.move("U")
+    print("moved;")
+    for n, s in enumerate(pm.sides):
+        print(s.piece_side, n)
+    print("<<p")
