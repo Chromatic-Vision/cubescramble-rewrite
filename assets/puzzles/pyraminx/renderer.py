8c225d2273105aff91128510b379a81b9dc2280a
huu-bo <huubhoogweg@gmail.com>
2023-12-06 21:10:12 +0100

    started pyraminx renderer

--- assets/puzzles/pyraminx/renderer.py
+++ assets/puzzles/pyraminx/renderer.py
@@ -5,25 +5,33 @@ from assets.puzzles.pyraminx.emulator import PyraminxEmulator
 
 
 SCALE = 100
+BORDER = 10
 Tri = list[list[float]]
-# TODO: renderer is not done yet
-
 
 class PyraminxRenderer(AbstractPuzzleRenderer):
     def __init__(self, puzzle):
         self._puzzle: PyraminxEmulator = puzzle
 
     def render(self, screen: pygame.Surface, pos: pygame.Rect) -> None:
-        x = screen.get_size()[0] - 395
-        y = screen.get_size()[1] - 200
+        x = screen.get_size()[0] - 300
+        y = screen.get_size()[1] - 300
 
         # background
         pygame.draw.rect(screen, (75, 75, 75), (x, y, x + 400, y + 200))
 
-        tris = piraminx_triangles(False)
-        for tri in tris:
-            pygame.draw.polygon(screen, (255, 255, 255),
-                                [(tri[i][0] * SCALE + x, tri[i][1] * SCALE + y) for i in range(3)], 2)
+        x += 25
+        y += 60
+
+        def draw_side(sx, sy, flipped):
+            tris = piraminx_triangles(flipped)
+            for tri in tris:
+                pygame.draw.polygon(screen, (255, 255, 255),
+                                    [(tri[i][0] * SCALE + x + sx, tri[i][1] * SCALE + y + sy) for i in range(3)], 2)
+
+        draw_side(0, SCALE - BORDER, False)
+        draw_side(SCALE // 2 + BORDER, 100 - BORDER * 2, True)
+        draw_side(SCALE + BORDER * 2, SCALE - BORDER, False)
+        draw_side(SCALE // 2 + BORDER, 0 - BORDER * 3, False)
 
 
 def _get_line_middle_coords(line: list[list[float]]):
@@ -75,11 +83,37 @@ def _get_tri_sub(tri: Tri) -> list[Tri]:
 
 
 def piraminx_triangles(flipped: bool) -> list[Tri]:
-    tri = [[0, 0], [1, 1], [-1, 1]]
+    # tri = [[0, 0], [1, 1], [-1, 1]]
+    tri = [[0.5, 0], [1, 1], [0, 1]]
     tris = _get_tri_sub(tri)
     out = []
     for t in tris:
         out += _get_tri_sub(t)
+
+    rm = []
+    for tri in out:
+        for p in tri:
+            if p[1] > .9:
+                rm.append(tri)
+    for tri in rm:
+        if tri in out:
+            out.remove(tri)
+
+    highest = 0
+    for tri in out:
+        for p in tri:
+            if p[1] > highest:
+                highest = p[1]
+
+    out = [[[p[0], p[1]] for p in tri] for tri in out]  # copy
+    for tri in out:
+        for p in tri:
+            p[0] *= 1 / highest
+            assert p[0] >= 0, p[0]
+            # print(p[1], highest)
+            assert p[1] <= highest
+            p[1] *= 1 / highest
+            # print(p[1])
 
     if flipped:
         for tri in out:
